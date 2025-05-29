c463ddac56eefe975141669d3ba01e6432cc5158
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-29 14:42:56 -0600

    feat: First Commit :-D

---
+++ Supermarket-inventory-System/src/main/java/com/mceit_z/Supermarket_inventory_System/SupermarketInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mceit_z.Supermarket_inventory_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketInventorySystemApplication.class, args);
+	}
+
+}
---
+++ Supermarket-inventory-System/src/test/java/com/mceit_z/Supermarket_inventory_System/SupermarketInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mceit_z.Supermarket_inventory_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
