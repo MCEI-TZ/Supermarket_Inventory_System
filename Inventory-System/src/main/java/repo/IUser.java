85cd9d74104c1a7ef413e1e9fca6c9b89f72db67
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-30 09:05:30 -0600

    feat(Repository): Add new Repository 🚧
    - Add interface for User Entity
    
    Implements: Repository

---
+++ Inventory-System/src/main/java/repo/IUser.java
@@ -0,0 +1,8 @@
+package repo;
+
+import models.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUser extends JpaRepository<UserProfile,Long> {
+    UserProfile findByUsername(String username);
+}
