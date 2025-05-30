2b7d5d298c75eeafdcfe3a26153dbf165452f732
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-30 14:10:45 -0600

    feat(services): Add new Service 🚧
    - Add InventoryMovement interface
    
    Author: MCEI-TZ

---
+++ Inventory-System/src/main/java/services/InventoryMovement/InventoryMovementService.java
@@ -0,0 +1,7 @@
+package services.InventoryMovement;
+
+import models.InventoryMovements;
+import services.generic.GenericService;
+
+public interface InventoryMovementService extends GenericService<InventoryMovements,Long> {
+}
