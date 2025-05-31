17588214d1ac97c16f5528dbcb69b1f53563a6fb
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-31 17:28:54 -0600

    feat(DTO): Add UserCreateDTO 🚧

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/user/UserCreateDTO.java
@@ -0,0 +1,27 @@
+package com.mceit_z.Inventory_System.dto.user;
+
+import com.mceit_z.Inventory_System.models.Branch;
+import com.mceit_z.Inventory_System.models.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDTO {
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String fullName;
+
+    private Set<Rol> roles = new HashSet<>();
+
+    private Branch branch;
+}
