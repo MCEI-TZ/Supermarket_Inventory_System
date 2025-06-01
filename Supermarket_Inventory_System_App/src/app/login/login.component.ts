448dc15b14e1190aed85ae669802d3ca1aa0850d
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-01 13:20:57 -0600

    feat: Integrate ToastService for success and error notifications in login process

--- Supermarket_Inventory_System_App/src/app/login/login.component.ts
+++ Supermarket_Inventory_System_App/src/app/login/login.component.ts
@@ -3,11 +3,12 @@ import { AuthService } from '../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ToastService } from '../services/core/toast.service.ts.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -15,17 +16,16 @@ export class LoginComponent {
   username = '';
   password = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastService: ToastService) {}
 
   onSubmit() {
     this.authService.login(this.username, this.password).subscribe({
       next: (response) => {
-        console.log('Login successful', response);
+        this.toastService.showSuccess('Login successful!', 'Success');
         this.router.navigate(['/dashboard']);
       },
       error: (error) => {
-        console.error('Login failed', error);
-        alert('Login failed. Please check your credentials.');
+        this.toastService.showError('Login failed. Please check your credentials.');
       },
     });
   }
