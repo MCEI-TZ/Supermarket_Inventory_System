440044ca239a5aba3a9a0e59542bb09cf0c1d563
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-31 19:48:08 -0600

    feat: Add DTO classes for product stock creation, reading, and updating

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/ProductStock/ProductStockUpdateDTO.java
@@ -0,0 +1,24 @@
+package com.mceit_z.Inventory_System.dto.ProductStock;
+
+import com.mceit_z.Inventory_System.models.Branch;
+import com.mceit_z.Inventory_System.models.Product;
+import com.mceit_z.Inventory_System.models.Supplier;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductStockUpdateDTO {
+    private Long idProductStock;
+    private Product product;
+    private Branch branch;
+    private Supplier supplier;
+    private int quantity;
+    private Integer stock;
+    private double price;
+    private Date lastRestockDate;
+}
