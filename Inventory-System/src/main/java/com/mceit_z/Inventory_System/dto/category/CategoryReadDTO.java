ff24336384123b74f901cc682327caea53dd591c
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-31 19:42:37 -0600

    feat: Add DTO classes for category creation, reading, and updating

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/category/CategoryReadDTO.java
@@ -0,0 +1,14 @@
+package com.mceit_z.Inventory_System.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryReadDTO {
+    private String name;
+
+    private String descripcion;
+}
---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/category/CategoryUpdateDTO.java
@@ -0,0 +1,16 @@
+package com.mceit_z.Inventory_System.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryUpdateDTO {
+    private Long idCategory;
+
+    private String name;
+
+    private String descripcion;
+}
