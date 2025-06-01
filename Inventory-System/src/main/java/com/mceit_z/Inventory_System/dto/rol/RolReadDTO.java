2833286e297db4127dce75b19701fe8eb8d7226a
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-31 19:56:07 -0600

    feat: Add DTO classes for role creation, reading, and updating

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/rol/RolReadDTO.java
@@ -0,0 +1,12 @@
+package com.mceit_z.Inventory_System.dto.rol;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolReadDTO {
+    private String name;
+}
