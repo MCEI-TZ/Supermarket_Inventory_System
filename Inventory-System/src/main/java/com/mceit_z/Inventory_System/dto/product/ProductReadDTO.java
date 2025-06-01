35b617c94e5ed4bb40475218cbe98667e296d586
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-05-31 19:49:57 -0600

    feat: Add DTO classes for product creation, reading, and updating

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/dto/product/ProductReadDTO.java
@@ -0,0 +1,23 @@
+package com.mceit_z.Inventory_System.dto.product;
+
+import com.mceit_z.Inventory_System.models.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductReadDTO {
+    private String name;
+
+    private String description;
+
+    private String barcode;
+
+    private Date expirationDate;
+
+    private Category category;
+}
