b482d4beed1dd45e7ec583af8faa5d2aeeeba16d
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-01 13:20:14 -0600

    feat: Configure Toastr notifications and add error interceptor in app config

--- Supermarket_Inventory_System_App/src/app/app.config.ts
+++ Supermarket_Inventory_System_App/src/app/app.config.ts
@@ -4,10 +4,24 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { errorInterceptor } from './interceptors/core/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'decreasing',
+      easeTime: 300,
+      tapToDismiss: true,
+    }),
   ],
 };
