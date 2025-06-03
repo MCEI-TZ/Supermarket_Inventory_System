a6d20d231c4745264624d394c00b9699eacb6c01
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-03 16:34:15 -0600

    feat: Create RolService interface for role management operations

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/services/rol/RolService.java
@@ -0,0 +1,7 @@
+package com.mceit_z.Inventory_System.services.rol;
+
+import com.mceit_z.Inventory_System.models.Rol;
+import com.mceit_z.Inventory_System.services.generic.GenericService;
+
+public interface RolService extends GenericService<Rol,Long> {
+}
