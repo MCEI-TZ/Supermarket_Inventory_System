0dc9e943870b911dbf6c3bf6691031f7e74c8ee5
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-03 16:41:56 -0600

    feat: Add UserProfileService interface for user profile management

---
+++ Inventory-System/src/main/java/com/mceit_z/Inventory_System/services/user/UserProfileService.java
@@ -0,0 +1,7 @@
+package com.mceit_z.Inventory_System.services.user;
+
+import com.mceit_z.Inventory_System.models.UserProfile;
+import com.mceit_z.Inventory_System.services.generic.GenericService;
+
+public interface UserProfileService extends GenericService<UserProfile, Long> {
+}
