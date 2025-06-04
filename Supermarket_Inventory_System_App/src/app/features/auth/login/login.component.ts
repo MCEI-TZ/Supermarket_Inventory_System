240db66e499a198724991fa97ce657c2978bd68b
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-04 12:48:21 -0600

    feat: Implement LoginComponent for user authentication with form handling and styling

---
+++ Supermarket_Inventory_System_App/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../core/auth/services/auth/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ToastService } from '../../../core/auth/services/toast/toast.service.ts.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router, private toastService: ToastService) {}
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        this.toastService.showSuccess('Login successful!', 'Success');
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        this.toastService.showError('Login failed. Please check your credentials.');
+      },
+    });
+  }
+}
