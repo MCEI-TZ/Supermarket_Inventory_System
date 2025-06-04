ff37b849b8b5a042df6bafaba5b02178af87169c
MCEI-TZ <144301860+MexzjhaCyEI@users.noreply.github.com>
2025-06-04 12:44:11 -0600

    feat: Add Branch model for representing branch details in the application

---
+++ Supermarket_Inventory_System_App/src/app/core/models/branch.model.ts
@@ -0,0 +1,9 @@
+export class Branch {
+  constructor(
+    public idBranch: number,
+    public name: string,
+    public address: string,
+    public city: string,
+    public region: string
+  ) {}
+}
